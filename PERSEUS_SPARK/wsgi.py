24399b97e7a0f601d80e91be60d09616d1a18ddb
Di Jin <xutong.jin@gmail.com>
2017-03-15 22:34:28 -0400

    first commit

---
+++ PERSEUS_SPARK/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for PERSEUS_SPARK project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PERSEUS_SPARK.settings")
+
+application = get_wsgi_application()
---
+++ README.md
@@ -0,0 +1,2 @@
+# PERSEUS_frontend
+The repository is for the frontend of project PERSEUS_Spark.
